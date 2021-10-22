fd238f26f39fe5e216c327204a5ade5033a65cc4
tarrmmy <tarrmmy@gmail.com>
2021-10-22 18:00:47 +0100

    covid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import Navbar from './componenets/Navbar.js'
+import Cards from './componenets/Card.js'
+import WorldTable from './componenets/WorldTable.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar/>
+     <Cards/>
+     <WorldTable/> 
     </div>
   );
 }
